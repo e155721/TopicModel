8556f4d1b6c632ac2ba0106b30a9c41369ef4b27
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-04-14 23:21:48 +0900

    Add plot.pdf

--- src/execution.R
+++ src/execution.R
@@ -7,7 +7,7 @@ input_text <- read_file("../data/PubMed/p")
 input_text <- clean(input_text)
 topics <- top_terms_by_topic_LDA(input_text, number_of_topics = 5)
 
-top_terms_by_topic_LDA(p, number_of_topics = 5)
+top_terms_by_topic_LDA(topics, number_of_topics = 5)
 
 pdf(file = "plot.pdf")
 plot(topics)
