9217b25cc8ff328f794501b325f059aa034fdc84
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-04-14 23:18:11 +0900

    Modify execution.R

--- src/execution.R
+++ src/execution.R
@@ -2,10 +2,12 @@ source("top_terms_by_topic_LDA.R")
 source("readFile.R")
 source("clean.R")
 
-input_text <- read_csv("../data/PubMed/papers.csv")
-input_text <- input_text$Title
+input_text <- read_file("../data/PubMed/p")
+#input_text <- input_text$Title
 input_text <- clean(input_text)
 topics <- top_terms_by_topic_LDA(input_text, number_of_topics = 5)
+
+top_terms_by_topic_LDA(p, number_of_topics = 5)
 
 pdf(file = "plot.pdf")
 plot(topics)
