85caddf12751c574bf224e73fdfa52a564efdc77
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-04-15 22:25:24 +0900

    Make graph without custom stop word.

--- src/execution.R
+++ src/execution.R
@@ -3,9 +3,10 @@ source("readFile.R")
 source("clean.R")
 
 input_text <- read_file("../data/PubMed/a")
-input_text <- clean(input_text, sw = c("oper", "author"))
+#input_text <- clean(input_text, sw = c("oper", "author"))
+input_text <- clean(input_text)
 topics <- top_terms_by_topic_LDA(input_text, number_of_topics = 2)
 
-pdf(file = "plot.pdf")
+pdf(file = "Rplot.pdf")
 plot(topics)
 dev.off()
