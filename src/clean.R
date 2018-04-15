28d80071b8b21a3a5cd24fb4ff7a0ae9d09aec45
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-04-15 12:54:50 +0900

    Update graph.

--- src/clean.R
+++ src/clean.R
@@ -1,27 +1,27 @@
 # read in our data
 
-clean <- function(input_text, sw = c("a")) 
+clean <- function(input_text, sw = c("a"))
 {
   # create a document term matrix to clean
-  reviewsCorpus <- Corpus(VectorSource(input_text)) 
+  reviewsCorpus <- Corpus(VectorSource(input_text))
   reviewsDTM <- DocumentTermMatrix(reviewsCorpus)
-  
+
   # convert the document term matrix to a tidytext corpus
   reviewsDTM_tidy <- tidy(reviewsDTM)
-  
+
   # I'm going to add my own custom stop words that I don't think will be
   # very informative in hotel reviews
   ## custom_stop_words <- tibble(word = c("hotel", "room"))
+  sw = c("author", "oper", "function", "predomin")
   custom_stop_words <-tibble(word = sw)
-  print(sw)
-  
+
   # remove stopwords
   reviewsDTM_tidy_cleaned <- reviewsDTM_tidy %>% # take our tidy dtm and...
     anti_join(stop_words, by = c("term" = "word")) %>% # remove English stopwords and...
     anti_join(custom_stop_words, by = c("term" = "word")) # remove my custom stopwords
-  
+
   # stem the words (e.g. convert each word to its stem, where applicable)
-  reviewsDTM_tidy_cleaned <- reviewsDTM_tidy_cleaned %>% 
+  reviewsDTM_tidy_cleaned <- reviewsDTM_tidy_cleaned %>%
     mutate(stem = wordStem(term))
   input_text <- reviewsDTM_tidy_cleaned$stem
   return(input_text)
--- src/execution.R
+++ src/execution.R
@@ -4,7 +4,7 @@ source("clean.R")
 
 input_text <- read_file("../data/PubMed/a")
 #input_text <- input_text$Title
-input_text <- clean(input_text, sw = c("author", "oper", "function", "predomin"))
+input_text <- clean(input_text)
 topics <- top_terms_by_topic_LDA(input_text, number_of_topics = 2)
 
 pdf(file = "plot.pdf")
