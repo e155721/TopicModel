36421cb3a01ff34a98ba07dacbd459069073df6f
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-04-16 17:52:46 +0900

    Update graph.

--- src/execution.R
+++ src/execution.R
@@ -2,9 +2,12 @@ source("top_terms_by_topic_LDA.R")
 source("readFile.R")
 source("clean.R")
 
-input_text <- read_file("../data/a.txt")
+#input_text <- read_file("../data/a")
+input_text <- read_file("../data/a-200000.txt")
 input_text <- clean(input_text)
-topics <- top_terms_by_topic_LDA(input_text, number_of_topics = 2)
+
+lda <- top_terms_by_topic_LDA(input_text, number_of_topics = 2, plot = F)
+topics <- top_terms_by_topic_LDA(input_text, number_of_topics = 2, user_model = lda)
 
 pdf(file = "../plot/Rplot.pdf")
 plot(topics)
