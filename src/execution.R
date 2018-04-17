3a633d333b28b980363df0b9aa0917a8b747d89f
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-04-17 16:36:14 +0900

    Add graph.

--- src/execution.R
+++ src/execution.R
@@ -11,7 +11,7 @@ lda <- top_terms_by_topic_LDA(input_text, number_of_topics = 2, plot = F)
 
 if(1){
 file_path <- "../data/"
-file_name <- "allergy-and-psychiatry_2000"
+file_name <- "allergy-and-psychiatry_2"
 plot_path <- "../plot/"
 
 file_path <- paste(file_path, file_name, sep = "")
@@ -19,14 +19,14 @@ input_text <- read_file(file_path)
 input_text <- clean(input_text)
 
 topics <- top_terms_by_topic_LDA(input_text, number_of_topics = 2, plot = T)
-plot_path <- paste(plot_path, "Rplot_", file_name, ".pdf", sep = "")
-pdf(file = plot_path)
+path <- paste(plot_path, file_name, ".pdf", sep = "")
+pdf(file = path)
 plot(topics)
 dev.off()
 
 topics <- top_terms_by_topic_LDA(input_text, user_model = lda)
-plot_path = paste(plot_path, "Rplot_", file_name, "_used-model", ".pdf", sep = "")
-pdf(file = plot_path)
+path = paste(plot_path, file_name, "_used-model", ".pdf", sep = "")
+pdf(file = path)
 plot(topics)
 dev.off()
 }
