16f20dac681f8b521569ab40f6bd71a8a300653c
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-04-17 16:41:01 +0900

    Add graph.

--- src/execution.R
+++ src/execution.R
@@ -9,7 +9,7 @@ input_text <- clean(input_text)
 lda <- top_terms_by_topic_LDA(input_text, number_of_topics = 2, plot = F)
 }
 
-if(1){
+if(0){
 file_path <- "../data/"
 file_name <- "allergy-and-psychiatry_2"
 plot_path <- "../plot/"
@@ -30,3 +30,11 @@ pdf(file = path)
 plot(topics)
 dev.off()
 }
+
+file_path <- "../data/"
+file_name <- "allergy-and-psychiatry_200000"
+plot_path <- "../plot/"
+
+file_path <- paste(file_path, file_name, sep = "")
+input_text <- read_file(file_path)
+input_text <- clean(input_text)
