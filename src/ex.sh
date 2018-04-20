4f33e6916b78d8a6f6255ee1e58229d5120686ef
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-04-20 10:37:22 +0900

    Update ex.sh

--- src/ex.sh
+++ src/ex.sh
@@ -1,4 +1,5 @@
 #!/bin/zsh
 
 file=$1
-<"$file" | sed -e 's/@//g' | sed -e 's/^/@/' | sed -e 's/^@[ ]//' | sed -e 's/^[ ]*//' | tr '\n' ' ' | tr '@' '\n' | grep \^'AB' >"$file:r".clean
+<"$file" | sed -e 's/@//g' | sed -e 's/^/@/' | sed -e 's/^@[ ]//' | sed -e 's/^[ ]*//' | tr '\n' ' ' | tr '@' '\n' | grep \^'AB' | sed -e 's/AB  - //' >"$file:r".clean
+rm "$file"
