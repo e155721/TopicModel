d95c144118e0be92d7e029ca8386cb638f0446f0
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-04-23 22:04:07 +0900

    Modify execution.R

--- src/execution.R
+++ src/execution.R
@@ -9,7 +9,7 @@ if(0) {
   lda <- top_terms_by_topic_LDA(input_text, number_of_topics = 2, plot = F)
 }
 
-if(0) {
+if(1) {
   file_path <- "../data/"
   file_name <- "allergy"
   plot_path <- "../plot/"
@@ -26,22 +26,4 @@ if(0) {
     plot(topics)
     dev.off()
   }
-}
-
-if(1) {
-  file_path <- "../data/"
-  file_name <- "a.csv"
-  plot_path <- "../plot/"
-  
-  file_path <- paste(file_path, file_name, sep = "")
-  input_text <- read_file(file_path)
-  input_text <- clean(input_text)
-  
-  i <- 2
-  topics <- top_terms_by_topic_LDA(input_text, number_of_topics = i, plot = T)
-  # topics <- top_terms_by_topic_LDA(input_text, number_of_topics = 2, plot = T, user_model = lda) 
-  path <- paste(plot_path, file_name, "k_", i, ".pdf", sep = "")
-  pdf(file = path)
-  plot(topics)
-  dev.off()
 }
