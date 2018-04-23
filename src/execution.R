ede0a5340c7c81cba4f89d5f2061e77252fb028f
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-04-23 23:56:20 +0900

    Modify execution.R

--- src/execution.R
+++ src/execution.R
@@ -34,19 +34,18 @@ if(0) {
 
 if(1) {
   file_path <- "../data/"
-  file_name <- "alg"
+  #file_name <- "alg"
+  file_name <- "allergy"
   plot_path <- "../plot/"
   
   file_path <- paste(file_path, file_name, sep = "")
   input_text <- read_file(file_path)
   input_text <- clean(input_text)
   
-  for (i in 2:20) {
-    topics <- top_terms_by_topic_LDA(input_text, number_of_topics = i, plot = T)
-    # topics <- top_terms_by_topic_LDA(input_text, number_of_topics = 2, plot = T, user_model = lda) 
-    path <- paste(plot_path, file_name, "_k_", i, ".pdf", sep = "")
-    pdf(file = path)
-    plot(topics)
-    dev.off()
+  list_of_lda <- list(19)
+  for (i in 2:10) {
+    list_of_lda[i-1] <- top_terms_by_topic_LDA(input_text, number_of_topics = i, plot = F)
   }
+  
+  save.image("list_of_lda.RData")
 }
