096a67dcc9299c6831c337cdfa530fc1b5c433cc
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-04-23 21:44:43 +0900

    Modify execution.R

--- src/execution.R
+++ src/execution.R
@@ -18,12 +18,14 @@ if(1) {
   input_text <- read_file(file_path)
   input_text <- clean(input_text)
   
-  topics <- top_terms_by_topic_LDA(input_text, number_of_topics = 3, plot = T)
-  # topics <- top_terms_by_topic_LDA(input_text, number_of_topics = 2, plot = T, user_model = lda) 
-  path <- paste(plot_path, file_name, ".pdf", sep = "")
-  pdf(file = path)
-  plot(topics)
-  dev.off()
+  for (i in 2:10) {
+    topics <- top_terms_by_topic_LDA(input_text, number_of_topics = i, plot = T)
+    # topics <- top_terms_by_topic_LDA(input_text, number_of_topics = 2, plot = T, user_model = lda) 
+    path <- paste(plot_path, file_name, "k_", i, ".pdf", sep = "")
+    pdf(file = path)
+    plot(topics)
+    dev.off()
+  }
 }
 
 if(0){
