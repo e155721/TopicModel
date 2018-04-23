eb1e6ff32c70404f5d2f96226f8d15e43bab8226
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-04-23 21:54:48 +0900

    Modify top_terms_by_topic_LDA.R and execution.R

--- src/execution.R
+++ src/execution.R
@@ -9,7 +9,7 @@ if(0) {
   lda <- top_terms_by_topic_LDA(input_text, number_of_topics = 2, plot = F)
 }
 
-if(1) {
+if(0) {
   file_path <- "../data/"
   file_name <- "allergy"
   plot_path <- "../plot/"
@@ -28,23 +28,19 @@ if(1) {
   }
 }
 
-if(0){
+if(1) {
   file_path <- "../data/"
-  file_name <- "allergy-and-psychiatry_2"
+  file_name <- "a.csv"
   plot_path <- "../plot/"
   
   file_path <- paste(file_path, file_name, sep = "")
   input_text <- read_file(file_path)
   input_text <- clean(input_text)
   
-  topics <- top_terms_by_topic_LDA(input_text, number_of_topics = 2, plot = T)
-  path <- paste(plot_path, file_name, ".pdf", sep = "")
-  pdf(file = path)
-  plot(topics)
-  dev.off()
-  
-  topics <- top_terms_by_topic_LDA(input_text, user_model = lda)
-  path = paste(plot_path, file_name, "_used-model", ".pdf", sep = "")
+  i <- 2
+  topics <- top_terms_by_topic_LDA(input_text, number_of_topics = i, plot = T)
+  # topics <- top_terms_by_topic_LDA(input_text, number_of_topics = 2, plot = T, user_model = lda) 
+  path <- paste(plot_path, file_name, "k_", i, ".pdf", sep = "")
   pdf(file = path)
   plot(topics)
   dev.off()
