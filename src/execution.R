85f846ceedfd5b7363e3b460bf6b66bf1c5d42e0
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-04-24 19:52:59 +0900

    Update execution.R

--- src/execution.R
+++ src/execution.R
@@ -4,6 +4,25 @@ source("clean.R")
 
 if(1) {
   file_path <- "../data/"
+  file_name <- "allergy_wiki"
+  plot_path <- "../plot/"
+  
+  file_path <- paste(file_path, file_name, sep = "")
+  input_text <- read_file(file_path)
+  input_text <- clean(input_text)
+  
+  for (i in 2:20) {
+    topics <- top_terms_by_topic_LDA(input_text, number_of_topics = i, plot = T)
+    #topics <- top_terms_by_topic_LDA(input_text, number_of_topics = 2, plot = T, user_model = lda)
+    path <- paste(plot_path, file_name, "_k_", i, ".pdf", sep = "")
+    pdf(file = path)
+    plot(topics)
+    dev.off()
+  }
+}
+
+if(0) {
+  file_path <- "../data/"
   file_name <- "allergy"
   plot_path <- "../plot/"
   
