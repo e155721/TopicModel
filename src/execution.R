fc8c357051462d483d89836566dc12da3f9e71c8
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-04-24 11:06:16 +0900

    Add graph.

--- src/execution.R
+++ src/execution.R
@@ -5,7 +5,7 @@ source("clean.R")
 if(0) {
   input_text <- read_file("../data/allergy-and-psychiatry_200000")
   input_text <- clean(input_text)
-  
+
   lda <- top_terms_by_topic_LDA(input_text, number_of_topics = 2, plot = F)
 }
 
@@ -13,11 +13,11 @@ if(0) {
   file_path <- "../data/"
   file_name <- "allergy"
   plot_path <- "../plot/"
-  
+
   file_path <- paste(file_path, file_name, sep = "")
   input_text <- read_file(file_path)
   input_text <- clean(input_text)
-  
+
   list_of_topics <- list(10)
   for (i in 11:20) {
     topics <- top_terms_by_topic_LDA(input_text, number_of_topics = i, plot = T)
@@ -28,26 +28,26 @@ if(0) {
     dev.off()
     list_of_topics[i] <- topics
   }
-  
+
   save.image("list_of_topics-2.RData")
 }
 
 if(1) {
-  
+
   file_path <- "../data/"
   #file_name <- "alg"
   file_name <- "allergy"
   plot_path <- "../plot/"
-  
+
   file_path <- paste(file_path, file_name, sep = "")
   input_text <- read_file(file_path)
   input_text <- clean(input_text)
-  
-  i <- 30
+
+  i <- 20
   #topics <- top_terms_by_topic_LDA(input_text, number_of_topics = i)
   topics <- top_terms_by_topic_LDA(input_text, number_of_topics = i, user_alpha = 3.99923)
-  path <- paste(plot_path, file_name, "_k_", i, ".pdf", sep = "")
-  pdf(file = path, width = 300, height = 300)
+  path <- paste(plot_path, file_name, "_k_", i, ".bmp", sep = "")
+  bmp(file = path, width = 1000, height = 1000)
   plot(topics)
   dev.off()
 }
@@ -59,11 +59,11 @@ if(0) {
   file_name <- "alg"
   #file_name <- "allergy"
   plot_path <- "../plot/"
-  
+
   file_path <- paste(file_path, file_name, sep = "")
   input_text <- read_file(file_path)
   input_text <- clean(input_text)
-  
+
   if(0) {
     list_of_psychiatry_lda <- list(9)
     # list_of_allergy_lda <- list(9)
@@ -72,8 +72,8 @@ if(0) {
       list_of_psychiatry_lda[i-1] <- top_terms_by_topic_LDA(input_text, number_of_topics = i, plot = F)
     }
   }
-  
-  
+
+
   if(1) {
     for (i in 2:10) {
       #topics <- top_terms_by_topic_LDA(input_text, user_model = list_of_allergy_lda[[i-1]])
