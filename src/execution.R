4dbb3b33b2ed3cb06cf2591c3f363b8baf4972b8
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-04-24 10:13:37 +0900

    Update graph.

--- src/execution.R
+++ src/execution.R
@@ -46,7 +46,7 @@ if(1) {
   i <- 20
     topics <- top_terms_by_topic_LDA(input_text, number_of_topics = i)
     path <- paste(plot_path, file_name, "_k_", i, ".pdf", sep = "")
-    pdf(file = path, width = 1000, height = 1000)
+    pdf(file = path, width = 500, height = 500)
     plot(topics)
     dev.off()
 }
