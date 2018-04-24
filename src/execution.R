18f340244b1eafca2f3e1cee38041dcfb92e8392
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-04-24 10:06:46 +0900

    Clean execution.R

--- src/execution.R
+++ src/execution.R
@@ -5,7 +5,7 @@ source("clean.R")
 if(0) {
   input_text <- read_file("../data/allergy-and-psychiatry_200000")
   input_text <- clean(input_text)
-
+  
   lda <- top_terms_by_topic_LDA(input_text, number_of_topics = 2, plot = F)
 }
 
@@ -13,11 +13,11 @@ if(0) {
   file_path <- "../data/"
   file_name <- "allergy"
   plot_path <- "../plot/"
-
+  
   file_path <- paste(file_path, file_name, sep = "")
   input_text <- read_file(file_path)
   input_text <- clean(input_text)
-
+  
   list_of_topics <- list(10)
   for (i in 11:20) {
     topics <- top_terms_by_topic_LDA(input_text, number_of_topics = i, plot = T)
@@ -28,22 +28,22 @@ if(0) {
     dev.off()
     list_of_topics[i] <- topics
   }
-
+  
   save.image("list_of_topics-2.RData")
 }
 
 if(0) {
   load("../data/list_of_lda.RData")
-
+  
   file_path <- "../data/"
   file_name <- "alg"
   #file_name <- "allergy"
   plot_path <- "../plot/"
-
+  
   file_path <- paste(file_path, file_name, sep = "")
   input_text <- read_file(file_path)
   input_text <- clean(input_text)
-
+  
   for (i in 2:10) {
     topics <- top_terms_by_topic_LDA(input_text, user_model = list_of_lda[[i-1]])
     path <- paste(plot_path, file_name, "_k_", i, ".pdf", sep = "")
@@ -60,33 +60,30 @@ if(1) {
   file_name <- "alg"
   #file_name <- "allergy"
   plot_path <- "../plot/"
-
+  
   file_path <- paste(file_path, file_name, sep = "")
   input_text <- read_file(file_path)
   input_text <- clean(input_text)
-
+  
   if(0) {
-  list_of_psychiatry_lda <- list(9)
-  # list_of_allergy_lda <- list(9)
-  for (i in 2:10) {
-  #list_of_allergy_lda[i-1] <- top_terms_by_topic_LDA(input_text, number_of_topics = i, plot = F)
-   list_of_psychiatry_lda[i-1] <- top_terms_by_topic_LDA(input_text, number_of_topics = i, plot = F)
+    list_of_psychiatry_lda <- list(9)
+    # list_of_allergy_lda <- list(9)
+    for (i in 2:10) {
+      #list_of_allergy_lda[i-1] <- top_terms_by_topic_LDA(input_text, number_of_topics = i, plot = F)
+      list_of_psychiatry_lda[i-1] <- top_terms_by_topic_LDA(input_text, number_of_topics = i, plot = F)
+    }
   }
-}
-
-
-if(1) {
-  for (i in 2:10) {
-    #topics <- top_terms_by_topic_LDA(input_text, user_model = list_of_allergy_lda[[i-1]])
-    #path <- paste(plot_path, file_name, "_use_model_allergy", "_k_", i, ".pdf", sep = "")
-    topics <- top_terms_by_topic_LDA(input_text, user_model = list_of_psychiatry_lda[[i-1]])
-    path <- paste(plot_path, file_name, "_use_model_psychiatry", "_k_", i, ".pdf", sep = "")
-    pdf(file = path)
-    plot(topics)
-    dev.off()
+  
+  
+  if(1) {
+    for (i in 2:10) {
+      #topics <- top_terms_by_topic_LDA(input_text, user_model = list_of_allergy_lda[[i-1]])
+      #path <- paste(plot_path, file_name, "_use_model_allergy", "_k_", i, ".pdf", sep = "")
+      topics <- top_terms_by_topic_LDA(input_text, user_model = list_of_psychiatry_lda[[i-1]])
+      path <- paste(plot_path, file_name, "_use_model_psychiatry", "_k_", i, ".pdf", sep = "")
+      pdf(file = path)
+      plot(topics)
+      dev.off()
+    }
   }
-}
-
-
-
 }
