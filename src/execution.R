22d697356261cb01ed907c7f64a45e7f5810b05d
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-04-27 23:16:13 +0900

    Modify exectution.R

--- src/execution.R
+++ src/execution.R
@@ -3,11 +3,11 @@ source("readFile.R")
 source("clean.R")
 
 if(1) {
-  load("../data/list_of_lda.RData")
+  #load("../data/list_of_lda.RData")
 
   file_path <- "../data/"
-  file_name <- "data.txt"
-  #file_name <- "pubmed_result.clean"
+  #file_name <- "data.txt"
+  file_name <- "pubmed_result.clean"
   #file_name <- "alg"
   #file_name <- "allergy_wiki"
   plot_path <- "../plot/"
@@ -17,9 +17,9 @@ if(1) {
   input_text <- clean(input_text)
 
   for (i in 2:10) {
-    #topics <- top_terms_by_topic_LDA(input_text, number_of_topics = i, plot = T)
+    #topics <- top_terms_by_topic_LDA(input_text, number_of_topics = i)
     #path <- paste(plot_path, file_name, "_unused", "_k_", i, ".pdf", sep = "")
-    topics <- top_terms_by_topic_LDA(input_text, number_of_topics = 2, plot = T, user_model = list_of_lda[[i-1]])
+    topics <- top_terms_by_topic_LDA(input_text, user_model = list_of_lda[[i-1]])
     path <- paste(plot_path, file_name, "_used", "_k_", i, ".pdf", sep = "")
     pdf(file = path)
     plot(topics)
