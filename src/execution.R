4c671676ef494a40b9a51bc56597f3dd9af88250
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-04-27 23:28:40 +0900

    Modify execution.R

--- src/execution.R
+++ src/execution.R
@@ -2,7 +2,7 @@ source("top_terms_by_topic_LDA.R")
 source("readFile.R")
 source("clean.R")
 
-if(1) {
+if(0) {
   #load("../data/list_of_lda.RData")
   
   file_path <- "../data/"
@@ -27,7 +27,7 @@ if(1) {
   }
 }
 
-if(0) {
+if(1) {
   file_path <- "../data/"
   file_name <- "data.txt"
   plot_path <- "../plot/"
@@ -36,11 +36,11 @@ if(0) {
   input_text <- read_file(file_path)
   input_text <- clean(input_text)
   
-  list_of_lda <- list(9)
-  for (i in 2:10) {
+  list_of_lda <- list(19)
+  for (i in 11:20) {
     lda <- top_terms_by_topic_LDA(input_text, number_of_topics = i, plot = F)
     # topics <- top_terms_by_topic_LDA(input_text, number_of_topics = 2, plot = T, user_model = lda)
     list_of_lda[i-1] <- lda
   }
-  save.image("list_of_lda.RData")
+  save.image("list_of_lda_11-20.RData")
 }
