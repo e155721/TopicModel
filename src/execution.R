97a540f33f1458ac71de519e1b1c298a6459b074
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-04-27 22:49:33 +0900

    Modify execution.R

--- src/execution.R
+++ src/execution.R
@@ -4,50 +4,41 @@ source("clean.R")
 
 if(0) {
   file_path <- "../data/"
-  file_name <- "pubmed_result.clean"
+  file_name <- "data.txt"
+  #file_name <- "pubmed_result.clean"
   #file_name <- "alg"
   #file_name <- "allergy_wiki"
-  #file_name <- "gundam_wiki"
   plot_path <- "../plot/"
-
+  
   file_path <- paste(file_path, file_name, sep = "")
   input_text <- read_file(file_path)
   input_text <- clean(input_text)
-
-  for (i in 2:20) {
-    #topics <- top_terms_by_topic_LDA(input_text, number_of_topics = i, plot = T)
-    #path <- paste(plot_path, file_name, "_unused", "_k_", i, ".pdf", sep = "")
+  
+  for (i in 2:10) {
+    topics <- top_terms_by_topic_LDA(input_text, number_of_topics = i, plot = T)
+    path <- paste(plot_path, file_name, "_unused", "_k_", i, ".pdf", sep = "")
     topics <- top_terms_by_topic_LDA(input_text, number_of_topics = 2, plot = T, user_model = list_of_lda[[i-1]])
-    path <- paste(plot_path, file_name, "_used", "_k_", i, ".pdf", sep = "")
+    #path <- paste(plot_path, file_name, "_used", "_k_", i, ".pdf", sep = "")
     pdf(file = path)
     plot(topics)
     dev.off()
   }
 }
 
-if(1) {
+if(0) {
   file_path <- "../data/"
   file_name <- "data.txt"
   plot_path <- "../plot/"
-
+  
   file_path <- paste(file_path, file_name, sep = "")
   input_text <- read_file(file_path)
   input_text <- clean(input_text)
-
+  
   list_of_lda <- list(9)
   for (i in 2:10) {
     lda <- top_terms_by_topic_LDA(input_text, number_of_topics = i, plot = F)
     # topics <- top_terms_by_topic_LDA(input_text, number_of_topics = 2, plot = T, user_model = lda)
-
-    if(0) {
-      path <- paste(plot_path, file_name, "_k_", i, ".pdf", sep = "")
-      pdf(file = path)
-      plot(topics)
-      dev.off()
-    }
-
     list_of_lda[i-1] <- lda
   }
-
   save.image("list_of_lda.RData")
 }
