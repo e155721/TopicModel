c734454e24091bd8f19b6e864e67618241f4a8d1
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-04-30 00:53:34 +0900

    Modify execution.R

--- src/execution.R
+++ src/execution.R
@@ -21,19 +21,17 @@ if(1) {
   
   list_of_top_terms <- list(9)
   
-  if (1) {
-    for (i in 2:10) {
-      #topics <- top_terms_by_topic_LDA(input_text, number_of_topics = i)
-      #path <- paste(plot_path, file_name, "_unused", "_k_", i, ".pdf", sep = "")
-      lda_result <- top_terms_by_topic_LDA(input_text, user_model = list_of_lda[[i-1]])
-      path <- paste(plot_path, file_name, "_used", "_k_", i, ".pdf", sep = "")
-      pdf(file = path)
-      plot(lda_result[[1]])
-      dev.off()
-      list_of_top_terms <- lda_result[[2]]
-    }
-    write.table(list_of_top_terms[[2]], file="top_terms.txt")
+  for (i in 2:10) {
+    #topics <- top_terms_by_topic_LDA(input_text, number_of_topics = i)
+    #path <- paste(plot_path, file_name, "_unused", "_k_", i, ".pdf", sep = "")
+    lda_result <- top_terms_by_topic_LDA(input_text, user_model = list_of_lda[[i-1]])
+    path <- paste(plot_path, file_name, "_used", "_k_", i, ".pdf", sep = "")
+    pdf(file = path)
+    plot(lda_result[[1]])
+    dev.off()
+    list_of_top_terms <- lda_result[[2]]
   }
+  write.table(list_of_top_terms[[2]], file="top_terms.txt")
 }
 
 if(0) {
