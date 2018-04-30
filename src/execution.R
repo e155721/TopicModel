20ae08e3f16c4f57c9a229b2854a987222fa75fd
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-04-30 17:38:46 +0900

    Add graphs.

--- src/execution.R
+++ src/execution.R
@@ -3,7 +3,7 @@ source("readFile.R")
 source("clean.R")
 
 if(1) {
-  load("../data/list_of_lda_02-10.RData")
+  #load("../data/list_of_lda_02-10.RData")
   #load("../data/list_of_lda_11-20.RData")
   #load("../data/list_of_lda.RData")
   
@@ -22,15 +22,15 @@ if(1) {
   input_text <- clean(input_text)
   
   for (i in 2:2) {
-    #lda_result <- top_terms_by_topic_LDA(input_text, number_of_topics = i)
-    #path <- paste(plot_path, file_name, "_unused_k_", i, ".pdf", sep = "")
-    lda_result <- top_terms_by_topic_LDA(input_text, user_model = list_of_lda[[i-1]])
-    path <- paste(plot_path, file_name, "_used_k_", i, ".pdf", sep = "")
+    lda_result <- top_terms_by_topic_LDA(input_text, number_of_topics = i)
+    path <- paste(plot_path, file_name, "_unused_k_", i, ".pdf", sep = "")
+    #lda_result <- top_terms_by_topic_LDA(input_text, user_model = list_of_lda[[i-1]])
+    #path <- paste(plot_path, file_name, "_used_k_", i, ".pdf", sep = "")
     pdf(file = path)
     plot(lda_result[[1]])
     dev.off()
-    #path <- paste(plot_path, file_name, "_unused_top_terms_k_", i, ".txt", sep = "")
-    path <- paste(plot_path, file_name, "_used_top_terms_k_", i, ".txt", sep = "")
+    path <- paste(plot_path, file_name, "_unused_top_terms_k_", i, ".txt", sep = "")
+    #path <- paste(plot_path, file_name, "_used_top_terms_k_", i, ".txt", sep = "")
     write.table(lda_result[[2]], file=path)
   }
 }
